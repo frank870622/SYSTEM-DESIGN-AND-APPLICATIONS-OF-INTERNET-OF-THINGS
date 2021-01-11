ece97fce24a62fd5c2f2a5f12e2fa7a06642171f
YuXiang <richardlin0212@gmail.com>
2021-01-11 23:41:51 +0800

    Add command to send

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from get_tello_ip import get_tello_ip
 import tello
+import time
 
 def main():
-    ip = get_tello_ip()
+    # ip = get_tello_ip()
+    ip = '172.20.10.13'
     mDrone = tello.Tello(tello_ip = ip)
+    
     mDrone.takePicture()
+    while True:
+        try:
+            a = input()
+            if a == 'a':
+                mDrone.takePicture()
+            elif a == 'b':
+                mDrone.command()
+                print("send command")
+        except KeyboardInterrupt:
+            break
+
     mDrone.stop()
 
 
